07e84262b7ead6df60ad93056e49c179949638c1
Andy McKay <amckay@mozilla.com>
2016-10-03 13:51:38 -0700

    a first sample extension

---
+++ api.js
@@ -0,0 +1,16 @@
+
+class API extends ExtensionAPI {
+  getAPI(context) {
+    return {
+      // Insert Experiment API here.
+      // Note: the namespace (boilerplate must match the id in the install.rdf)
+      /*
+      boilerplate: {
+        hello() {
+          return "Hello, world!";
+        },
+      },
+      */
+    };
+  }
+}
