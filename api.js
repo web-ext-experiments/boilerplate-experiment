a56e81f6a3fb735bffabe1c34d82548286df0fc1
Robert Helmer <rhelmer@rhelmer.org>
2017-02-10 17:14:55 -0800

    return async message from hello function

--- api.js
+++ api.js
@@ -1,16 +1,13 @@
-
 class API extends ExtensionAPI {
   getAPI(context) {
     return {
       // Insert Experiment API here.
       // Note: the namespace (boilerplate must match the id in the install.rdf)
-      /*
       boilerplate: {
-        hello() {
+        async hello() {
           return "Hello, world!";
-        },
-      },
-      */
+        }
+      }
     };
   }
 }
